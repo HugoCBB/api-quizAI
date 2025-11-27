11e229a27989aea510cab398975c8cc71a8b2ce9
Hugo <157296697+HugoCBB@users.noreply.github.com>
2025-11-26 22:31:01 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.quiz_routes import route as quiz_router
 from app.api.user_routes import route as user_router
 from dotenv import load_dotenv
@@ -7,10 +8,21 @@ from app.infra.database.db import Base, engine
 Base.metadata.create_all(bind=engine)
 
 load_dotenv()
+
+
 app = FastAPI()
 
 app.include_router(quiz_router, prefix="/question", tags=["Question"])
 app.include_router(user_router, prefix="/users", tags=["User"])
+
+origins = "http://127.0.0.1:5500/index.html"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def root():
