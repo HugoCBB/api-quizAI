5daa58dfa17a664f7d25f07e6143a187ad1a70ec
Hugo <157296697+HugoCBB@users.noreply.github.com>
2026-03-04 07:12:25 -0300

    Comment out database setup and user routes
    Comment out database initialization and user router inclusion.

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.quiz_routes import route as quiz_router
 from app.api.user_routes import route as user_router
 from dotenv import load_dotenv
-from app.infra.database.db import Base, engine
+#from app.infra.database.db import Base, engine
 
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 
 load_dotenv()
 
@@ -13,7 +13,7 @@ load_dotenv()
 app = FastAPI()
 
 app.include_router(quiz_router, prefix="/question", tags=["Question"])
-app.include_router(user_router, prefix="/users", tags=["User"])
+#app.include_router(user_router, prefix="/users", tags=["User"])
 
 origins = "https://quizai-kappa.vercel.app/"
 app.add_middleware(
